49792592477f9acd6c83da374df3cf970cb3c50e
Veer <veervanshajwadehra@gmail.com>
2025-07-21 11:05:40 +0530

    now also printing the input variable previsly forgot

--- Assignments/assignment-1/index.js
+++ Assignments/assignment-1/index.js
@@ -4,5 +4,5 @@ let a=process.argv[2];
 
 fs.writeFile("./writeByTerminal",a,function(err,data){
 if(err) return console.log(err)
-    console.log("data written on file by taking input by terminal")
+    console.log("data written on file by taking input by terminal ",a)
 })
